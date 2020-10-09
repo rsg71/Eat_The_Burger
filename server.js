076c499dc29ade1998320bf03c93b5b933384150
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-10-09 18:09:16 -0400

    added deleted functionality to devour burger button

--- server.js
+++ server.js
@@ -24,98 +24,77 @@ app.set("view engine", "handlebars");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "shed120",
-  database: "burger_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "shed120",
+    database: "burger_db"
 });
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
-  
+
     console.log("connected as id " + connection.threadId);
-  });
+});
 
 
-
-
-const burgerList = [
-    {
-        id: 1, 
-        name: "Bacon Burger",
-        devoured: false
-    },
-    {
-        id: 2, 
-        name: "Cheese Burger",
-        devoured: false
-    },
-    {
-        id: 3, 
-        name: "Turkey Burger",
-        devoured: false
-    }
-]
 
 // Routes = Controller =================================================================
+
+
+// Use Handlebars to render the main index.html page with the list of all the burgers
 app.get("/", (req, res) => {
 
-    connection.query("SELECT * FROM burgers;", function(err, data) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
         if (err) {
-          throw err;
+            throw err;
         }
-    
         // Test:
         console.log('The burgers are: ', data);
-    
         res.render("index", { burgers: data });
-   
-})
-
-});
-
-app.get("/api/burgers", (req, res) => {
-    //my SQL retrieves burgers array from DB
-    res.json(burgers);
-});
-
-app.put("/api/burgers/:id", (req,res) => {
-    const chosen = req.params.id;
-})
-
-
-app.post("/api/burgers", (req, res) => {
- // Test it.
-  console.log('You sent ' + req.body.newBurger);
-
-  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.newBurger], function(err, result) {
-    if (err) {
-      throw err;
-    }
-
-    res.redirect("/");
-  });
-
-})
-
-
-app.put("/update", (req, res) => {
-    connection.query("UPDATE burgers SET burger_name = ? WHERE id =?", [req.body.burger, req.params.id], function(err, result) {
-        if(err)
-        return console.log(err);
-
-        res.status(200).end();
     })
+
+});
+
+// app.get("/api/burgers", (req, res) => {
+//     //my SQL retrieves burgers array from DB
+//     res.json(burgers);
+// });
+
+
+// app.put("/api/burgers/:id", (req, res) => {
+//     const chosen = req.params.id;
+// })
+
+
+
+// Enter in a new burger
+app.post("/api/burgers", (req, res) => {
+    // Test it.
+    console.log('You sent ' + req.body.newBurger);
+
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.newBurger], function (err, result) {
+        if (err) {
+            throw err;
+        }
+        res.redirect("/");
+    });
 })
 
 
-app.delete("/delete", (req, res) => {
-    connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+// ==========================================================================
+//Remove burger from "undevoured" list and move it to the devoured list:
+// ==========================================================================
+
+//Remove from undevoured list
+app.delete("/devour/:id", (req, res) => {
+    connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function (err, result) {
+        console.log(req.params.id)
+
         if (err) return console.log(err);
 
         res.status(200).end();
@@ -123,9 +102,24 @@ app.delete("/delete", (req, res) => {
 })
 
 
+//Add it to the devoured list
+app.post("/api/burgers", (req, res) => {
+    // Test it.
+    console.log('You sent ' + req.body.newBurger);
+
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.newBurger], function (err, result) {
+        if (err) {
+            throw err;
+        }
+        res.redirect("/");
+    });
+})
+
+
+
+
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
-  
